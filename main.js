ef7e7ae5b67dd37440fbf0ed7367fddb860dd069
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-27 23:10:19 +0100

    scroll section active lnk done

--- main.js
+++ main.js
@@ -17,6 +17,13 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-    })
+        let id = sec.getAttribute('id'); 
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 }
