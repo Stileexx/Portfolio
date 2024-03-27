accbe1c5d2d22bc6ba77881d045729cb80dd8e2e
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-27 23:05:22 +0100

    onscrollY variables

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ menuIcon.onclick = () => {
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+    })
+}
